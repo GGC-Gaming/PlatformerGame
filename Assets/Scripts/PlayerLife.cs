816bbde0fbb3c022067b53c270e225f928bb38f3
Szavior McRae <szaviormcrae@gmail.com>
2022-04-28 13:58:06 -0400

    Implemented Lives and also resets to another scene

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -6,6 +6,8 @@ using UnityEngine.SceneManagement;
 public class PlayerLife : MonoBehaviour
 {
     private Rigidbody2D rigidB;
+    [SerializeField] public int maxPlayerLives = 3;
+    public static int currentPlayerLives = 3;
 
     // Start is called before the first frame update
     void Start()
@@ -22,13 +24,23 @@ public class PlayerLife : MonoBehaviour
         if (otherObject.name == "Player")
         {
 
-            // Resets the current scene to the beginning
-            RestartLevel();
+            // Resets the current scene to the beginning 
+            if (currentPlayerLives == 0)
+            {
+                SceneManager.LoadScene("JustinTestScene");
+                currentPlayerLives = maxPlayerLives;
+            }
+            else
+            {
+                RestartLevel();
+            }
+            Debug.Log(currentPlayerLives);
         }
     }
 
     private void RestartLevel()
     {
             SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            currentPlayerLives--;
     }
 }
